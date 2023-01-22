1ee56bedef3651390ec0414c0b0fdf1ee58dbde4
thomas <tom.angotti11@gmail.com>
2023-01-22 15:34:36 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import NavBar from './components/NavBar'
+import Home from './components/Home'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div className="App">
+      <NavBar />
+      <Home />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/RenderPost.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function RenderPost() {
+    return(
+        <div>
+            <img src="" alt="Profile image" />
+            <h3>User Name</h3>
+            <p>Post words, content, stuff, things , other stuff, your things, maybe images</p>
+        </div>
+    )
+}
+
+export default RenderPost;
